3cf2ff8578628d8022493f3ed9c0298ff2ccf793
nuuuwan <nuuuwan@gmail.com>
2025-05-11 07:24:43 +0530

    Updated remainder

---
+++ _scripts/git_pipeline.ps1
@@ -0,0 +1,30 @@
+git add ents
+git commit -m "Updated ents"
+
+git add gig2
+git commit -m "Updated gig2"
+
+git add gig2_custom_ec_only
+git commit -m "Updated gig2_custom_ec_only"
+
+git add _tmp
+git commit -m "Updated _tmp"
+
+git add geo\province
+git add geo\ed
+git add geo\district
+git add geo\pd
+git add geo\dsd
+git add geo\lg
+git add geo\moh
+git add geo\gnd
+git add geo
+
+git commit -m "Updated geo"
+
+git add *
+git commit -m "Updated remainder"
+
+git push origin master
+
+echo "Done!"
